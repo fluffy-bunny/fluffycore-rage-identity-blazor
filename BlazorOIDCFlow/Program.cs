af4dc9333aa4b18430aa4bf6dca35111607ec8b9
herb stahl <ghstahl@gmail.com>
2024-09-21 09:17:41 -0700

    pull appsettings.json to determine what services to load

--- BlazorOIDCFlow/Program.cs
+++ BlazorOIDCFlow/Program.cs
@@ -3,14 +3,33 @@ using BlazorOIDCFlow.Contracts;
 using BlazorOIDCFlow.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Text;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
+var httpClient = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
+var response = await httpClient.GetAsync("appsettings.json");
+var json = await response.Content.ReadAsStringAsync();
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonStream(new MemoryStream(Encoding.UTF8.GetBytes(json)))
+    .Build();
+
+var applicationEnvironment = configuration.GetValue<string>("ApplicationEnvironment");
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddScoped<IRageApiService, LocalRageApiService>();
+if (applicationEnvironment == "Production")
+{
+    builder.Services.AddScoped<IRageApiService, ProdRageApiService>();
+}
+else
+{
+    builder.Services.AddScoped<IRageApiService, LocalRageApiService>();
+}
 builder.Services.AddScoped<RedirectService>();
 
 await builder.Build().RunAsync();
