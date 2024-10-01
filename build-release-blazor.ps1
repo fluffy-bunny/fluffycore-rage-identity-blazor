e1aa7e032f0ce1677658f286dfc2de49c0fa1a7b
Herb Stahl <ghstahl@gmail.com>
2024-10-01 09:04:39 -0700

    update

--- build-release-blazor.ps1
+++ build-release-blazor.ps1
@@ -1,1 +1,2 @@
-dotnet build -c Release -o publish
+dotnet build BlazorOIDCFlow -c Release -o publish_BlazorOIDCFlow
+dotnet build BlazorAccountManagement -c Release -o publish_BlazorAccountManagement
