7c91338970d021f733d4dc3a16b2e1171ed90f3c
Herb Stahl <ghstahl@gmail.com>
2024-10-14 11:18:26 -0700

    cleanup

---
+++ BlazorAccountManagement/Contracts/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorAccountManagement.Contracts
+{
+    public class AppSettings
+    {
+        public string ApplicationEnvironment { get; set; }
+        public string BaseApiUrl { get; set; }
+        public string AppName { get; set; }
+        public string UnauthorizedRedirectUrl { get; set; }
+    }
+}
